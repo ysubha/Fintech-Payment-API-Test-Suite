5cd6e1da879fbdbc0d0fb51c4d1ebd9ab620fed5
Subha <ysubha920@gmail.com>
2026-03-16 09:59:58 +0530

    Unit Tests - part1

---
+++ tests/unit_tests/test_pay_engine_get_balance.py
@@ -0,0 +1,17 @@
+from utils.unit_tests_assertions import *
+
+
+def test_get_balance_successfully(pay_engine):
+    user_id = 'User001'
+    initial_balance = 1000
+    assert_create_user_successfully(pay_engine, user_id, initial_balance)
+    assert_current_balance(pay_engine, user_id, initial_balance)
+
+
+def test_get_balance_for_unknown_user_failure(pay_engine):
+    user_id = 'Unknown_user'
+
+    # Get Balance
+    failure_response = pay_engine.get_balance(user_id)
+    assert failure_response['status'] == 'FAILED'
+    assert failure_response['reason'] == 'USER_NOT_FOUND'
